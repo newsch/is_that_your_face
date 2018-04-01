c0e8329abd5cc0a171e7d3d8c5a5b66d9e880b8a
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2018-04-01 14:09:56 -0400

    Generalize createFisherModel for sets with arbitrary images and classes

--- FisherModel/createFisherModel.m
+++ FisherModel/createFisherModel.m
@@ -10,42 +10,37 @@ function [classedWeights,Classes,FisherFaces] = createFisherModel(faces, EigenFa
     [V,D] = eig(fCovarriance);
     AllEigenFaces = nFaces*fliplr(V);  % flip Eigenfaces so strongest is first
     EigenFaces = AllEigenFaces(:,1:limit);  % limit eigenfaces (and weights indirectly)
-%     allWeights = linsolve(EigenFaces,nFaces);  % generate weights for training set
-%     allWeights = EigenFaces\nFaces;
     allWeights = EigenFaces'*nFaces;
-    limitedWeights = allWeights(end-limit+1:end,:);
+
+    %% Arrange classes in preparation for Sb and Sw calculations
+    [Classes,ia,ic] = unique(classes);  % get unique names and their indices
+    numClasses = length(Classes);  % number of unique classes
+    numImages = length(classes);  % total number of images is the same as the total number of unfiltered class entries
+    numDims = length(allWeights(:,1));  % number of dimensions in Eigenspace we're working in (length of one image in Eigenspace)
     
     %% Find W_opt using Sb and Sw
     % Sb of faces in Eigenspace
-    AverageClassFaces = zeros(132,66);
-    for i = 1:66  % create average face for each class (person)
-        AverageClassFaces(:,i) = (allWeights(:,2*i)+allWeights(:,2*i-1))/2 ;
+    AverageClassFaces = zeros(numDims,numClasses);
+    for i = 1:length(Classes)  % create average face for each class (person)
+        AverageClassFaces(:,i) = mean(allWeights(:,ic==i),2);
     end
 
-    TotalAverageFace = zeros(132,1);
-    for i = 1:132  % create average face of all faces
-        TotalAverageFace = TotalAverageFace + allWeights(:,i);
-    end
-    TotalAverageFace = TotalAverageFace./132;
+    TotalAverageFace = mean(allWeights,2);  % create average face of all faces, not accounting for difference in class sizes
 
-    Sb = zeros(132,132);
+    Sb = zeros(numDims);
     for i = 1:66
-        Sb = Sb + (2*(AverageClassFaces(:,i)-TotalAverageFace)*(AverageClassFaces(:,i)-TotalAverageFace).') ;
+        numImagesInClass = sum(ic==i);
+        Sb = Sb + (numImagesInClass*(AverageClassFaces(:,i)-TotalAverageFace)*(AverageClassFaces(:,i)-TotalAverageFace).');
     end
 
     % Sw of faces in Eigenspace
-    Sw = zeros(132,132);
-    for i = 1:66
-        InsideWithin2 = zeros(132,132);
-        for k = 1:2
-            InsideWithin1 = (allWeights(:,2*i-2+k) - AverageClassFaces(:,i)) * (allWeights(:,2*i-2+k) - AverageClassFaces(:,i)).' ;
-            InsideWithin2 = InsideWithin2 + InsideWithin1;
-        end
-        Sw = Sw + InsideWithin2;
+    Sw = zeros(numDims);
+    for i = 1:numClasses  % calculate nested sums
+        innerSum = (allWeights(:,ic==i) - AverageClassFaces(:,i)) * (allWeights(:,ic==i) - AverageClassFaces(:,i))';
+        Sw = Sw + innerSum;
     end
     [Wopt,D] = eig(Sb,Sw);
-    %% organize by name and calculate average weights
-    [Classes,ia,ic] = unique(classes);  % get unique names and their indices
+    %% calculate average weights of classes
     FisherFaces = EigenFaces*Wopt;
     nFisherFaces = (FisherFaces - mean(FisherFaces,1)); %./sqrt(length(sFaces(:,1)));  % normalize faces
     fisherWeights = nFisherFaces'*stackim(faces);
@@ -54,8 +49,5 @@ function [classedWeights,Classes,FisherFaces] = createFisherModel(faces, EigenFa
         weightsToAverage = fisherWeights(:,ic == i);  % get all weights under the same class (person)
         classedWeights(:,i) = sum(weightsToAverage,2)./length(weightsToAverage(1,:));  % average weights to single vector
     end
-    
-    model.weights = classedWeights;
-    model.names = Classes;
-    model.eigenfaces = EigenFaces;
+
 end
