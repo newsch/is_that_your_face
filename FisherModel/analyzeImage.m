c6d4d7f5175e23239d794de7653db0a0d43a5ca9
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2018-04-01 11:11:45 -0400

    Add analyzeImage function for FisherModel

--- FisherModel/analyzeImage.m
+++ FisherModel/analyzeImage.m
@@ -1,4 +1,8 @@
-function [Distance, NameIndices] = analyzeImage(face,NamedWeights,FisherFaces)
+function [Distances, ClassIndices] = analyzeImage(face,ClassedWeights,FisherFaces)
 % ANALYZEIMAGE  Analyze an image with a Fisherface model.
-    return
+    sFace = stackim(face);
+    nFace = sFace - mean(sFace);  % subtract average of face from face
+    weights = model.fisherfaces'*nFace;  % express image in Fisherspace
+    unsorteddistances = sqrt(sum((ClassedWeights - weights).^2, 1));  % compute Euclidean distances
+    [Distances,ClassIndices] = sort(unsorteddistances);  % value and index of minimum values
 end
